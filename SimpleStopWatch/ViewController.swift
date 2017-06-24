adcb868eabd647a22d63d6b00a81f23f99e3a7f0
heyukun <1412805450@qq.com>
2017-06-24 16:35:38 +0800

    将reset图标换成文字

--- SimpleStopWatch/ViewController.swift
+++ SimpleStopWatch/ViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLable: UILabel!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var pauseBtn: UIButton!
+    
+    //播放状态标志
+    var isPlay: Bool!;
+    //计时器
+    var timer = Timer();
+    //count
+    var counter = 0.0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        counterLable.text = String(counter)
+        isPlay = false
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,45 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+   //开始
+    @IBAction func playTapped(_ sender: Any) {
+        if isPlay {
+            return
+        }
+        playBtn.isEnabled = false
+        pauseBtn.isEnabled = true
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.UpdateTimer), userInfo: nil, repeats: true)
+        
+        isPlay = true
+        
+        
+    }
+    //暂停
+    @IBAction func pauseTapped(_ sender: Any) {
+        if !isPlay{
+            return
+        }
+        playBtn.isEnabled = true
+        pauseBtn.isEnabled = false
+        timer.invalidate()
+        isPlay = false
+    }
+    
+    //复位
+    @IBAction func resetTapped(_ sender: Any) {
+        playBtn.isEnabled = true
+        pauseBtn.isEnabled = true
+        timer.invalidate()
+        counter = 0.0
+        counterLable.text = String(counter)
+        isPlay = false
+    }
+    
+    func UpdateTimer(){
+        counter = counter + 0.1
+        counterLable.text = String(counter)
+    }
+  
 
 }
 
